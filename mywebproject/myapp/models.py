3f439d9e7c1b41e6164d9f3699ac647722c6263b
byryepez <byryepez@gmail.com>
2022-03-11 21:45:39 -0700

    Creating models

--- mywebproject/myapp/models.py
+++ mywebproject/myapp/models.py
@@ -1,8 +1,15 @@
-from unittest.util import _MAX_LENGTH
+from tkinter import CASCADE
 from django.db import models
+from more_itertools import quantify
 
 # Create your models here.
 
 
 class Order(models.Model):
-    PO_number = models.Charfield(max_length = 30)
+    PO_number = models.CharField(max_length = 30)
+
+class Item(models.Model):
+    Order = models.ForeignKey(Order, on_delete=CASCADE)
+    quantity = models.IntegerField()
+    value = models.DecimalField()
+    
--- mywebproject/mywebproject/settings.py
+++ mywebproject/mywebproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1un9c(lv0i$x@@9@5d5)!97vt_6-j#c)@si-d@&kgg0@(f=+5l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default = False, cast = bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
